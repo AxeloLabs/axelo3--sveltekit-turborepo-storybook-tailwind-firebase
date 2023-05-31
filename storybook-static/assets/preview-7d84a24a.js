33260ad03c515a5a78dc85c535c1ff351b1ce24f
regnou <regnou.a@gmail.com>
2023-05-31 21:57:41 +0200

    commit 4 -- 31 may

--- storybook-static/assets/preview-7d84a24a.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-db477e4d.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-cdd67c9d.js"),["./DocsRenderer-EYKKDMVH-cdd67c9d.js","./iframe-db477e4d.js","./index-6d850120.js","./_commonjsHelpers-042e6b4d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-cbc7e548.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-7d84a24a.js.map
