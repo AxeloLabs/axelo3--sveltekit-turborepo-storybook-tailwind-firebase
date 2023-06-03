e11a0a895c4f8138631946e6ba01cd1b6bd8751b
regnou <regnou.a@gmail.com>
2023-06-03 16:01:56 +0200

    c7 - clean git understanding

---
+++ src/stories/z-storybook/tuto/TaskList.stories.ts
@@ -0,0 +1,58 @@
+import { MarginDecorator, PureTaskList } from '$components/widgets/demo/index.js';
+import * as TaskStories from './Task.stories';
+
+export default {
+	component: PureTaskList,
+	//👇 The auxiliary component will be added as a decorator to help show the UI correctly
+	decorators: [() => MarginDecorator],
+	title: '0 - demo/Tuto/B - TaskList',
+	argTypes: {
+		onPinTask: { action: 'onPinTask' },
+		onArchiveTask: { action: 'onArchiveTask' }
+	}
+};
+
+const Template = (args) => ({
+	Component: PureTaskList,
+	props: args,
+	on: {
+		...TaskStories.actionsData
+	}
+});
+export const Default = Template.bind({});
+Default.args = {
+	// Shaping the stories through args composition.
+	// The data was inherited from the Default story in task.stories.js.
+	tasks: [
+		{ ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+		{ ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+		{ ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+		{ ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+		{ ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+		{ ...TaskStories.Default.args.task, id: '6', title: 'Task 6' }
+	]
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+	// Shaping the stories through args composition.
+	// Inherited data coming from the Default story.
+	tasks: [
+		...Default.args.tasks.slice(0, 5),
+		{ id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' }
+	]
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+	tasks: [],
+	loading: true
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+	// Shaping the stories through args composition.
+	// Inherited data coming from the Loading story.
+	...Loading.args,
+	loading: false
+};
--- storybook-static/assets/Ax2HeaderApp.stories-3ac0b1c1.js
+++
@@ -1,7 +0,0 @@
-import{b as o,a as s}from"./Ax1FrameApp-f0e96775.js";import"./index-045b47e0.js";import"./AxHeaderMenuitem.svelte_svelte_type_style_lang-6c7218fd.js";import"./index-b7956fed.js";import"./AxHeaderMenuitem-c5fda2d3.js";const _={title:"frame/Ax2HeaderApp",component:o,tags:["autodocs"]},e={args:{active:"/acceuil",menus:s}};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    active: '/acceuil',
-    menus: AX_CONST__UI__menuApp
-  }
-}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const u=["Primary"];export{e as Primary,u as __namedExportsOrder,_ as default};
-//# sourceMappingURL=Ax2HeaderApp.stories-3ac0b1c1.js.map
